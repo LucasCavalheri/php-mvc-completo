74f914b660f795502d29612452f44c48f518f042
LucasCavalheri <cav.catecumenal@gmail.com>
2023-06-23 16:26:28 -0300

    feat: adicionando tratamento de erros com Whoops

--- public/index.php
+++ public/index.php
@@ -6,6 +6,11 @@ session_start();
 
 require '../vendor/autoload.php';
 
+$whoops = new \Whoops\Run;
+$whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
+$whoops->register();
+
+
 $app = new AppExtract;
 $controller = $app->controller();
 $method = $app->method();
@@ -15,6 +20,14 @@ $controller = new $controller;
 $controller->$method($params);
 
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    if (empty($controller->data)) {
+        throw new \Exception('Controller precisa ter uma propriedade $data');
+    }
+
+    if (!array_key_exists('title', $controller->data)) {
+        throw new \Exception('A propriedade $data precisa ter uma chave chamada title');
+    }
+
     $viewsPath = '../app/views/';
     extract($controller->data);
     require '../app/views/index.php';
